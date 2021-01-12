2124140adc49e40a64db08ef338853bfe0691946
Christian <cfa@innovait.dk>
2021-01-12 12:12:34 +0100

    Changed script to Mirror files

--- scripts/copyImageFiles.cmd
+++ scripts/copyImageFiles.cmd
@@ -1,5 +1,5 @@
-#run this locally to update images, css and so 
+#run this locally to mirror non-md files from "docs" to other folders 
 #this is done instead of having crowdin handle all the image files
-robocopy  "../docs" "../docs_da/docs" /S /XF *.md
-robocopy  "../docs" "../docs_de/docs" /S /XF *.md
-robocopy  "../docs" "../docs_ro/docs" /S /XF *.md
+robocopy  "../docs" "../docs_da/docs" /S /XF *.md /MIR
+robocopy  "../docs" "../docs_de/docs" /S /XF *.md /MIR
+robocopy  "../docs" "../docs_ro/docs" /S /XF *.md /MIR
