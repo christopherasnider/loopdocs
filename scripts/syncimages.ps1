c190ba21b7a292cf87c68c0c460e6de0b5c50a2f
Christian <cfa@innovait.dk>
2021-01-14 12:42:02 +0100

    Added images to mainsite
    Refactoring image-url - take 1

--- scripts/syncimages.ps1
+++ scripts/syncimages.ps1
@@ -3,3 +3,5 @@
 Get-ChildItem "../" | Where-Object{ $_.Name -like "docs_*" } | ForEach-Object { 
     robocopy "../docs" "$($_.FullName)\docs" /S /XF *.md /MIR
 } 
+
+robocopy "../docs" "../mainsite/docs/img" /S /XF *.md /MIR
