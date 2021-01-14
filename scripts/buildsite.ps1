b23a849be638d14e3c10dcc3312b809170cc555b
Christian <cfa@innovait.dk>
2021-01-14 15:34:45 +0100

    New script

--- scripts/buildsite.ps1
+++ scripts/buildsite.ps1
@@ -1,20 +1,22 @@
-$sitedir=".buildsite"
 
+$topdirectory=(get-item $PSScriptRoot).parent.FullName
+$sitedir="$($topdirectory)\.buildsite"
 
-mkdocs build --config-file ../mainsite/mkdocs.yml --site-dir ../$sitedir
+mkdocs build --config-file $topdirectory\mainsite\mkdocs.yml --site-dir $sitedir
 
-mkdocs build --config-file ../mkdocs_en.yml --site-dir $sitedir/en
-mkdocs build --config-file ../mkdocs_old.yml --site-dir $sitedir/en_old
+mkdocs build --config-file $topdirectory\mkdocs_en.yml --site-dir $sitedir\en
+mkdocs build --config-file $topdirectory\mkdocs_old.yml --site-dir $sitedir\en_old
 
 Get-ChildItem "../" | Where-Object{ $_.Name -like "docs_*" } | ForEach-Object { 
 
     $lang=$_.Name.Substring(5,2)
 
     $template=Get-Content -path "template_mkdocs.yml" -Raw
-    $nav=Get-Content -path "../$($_.Name)/docs/nav.yml" -Raw
+    $nav=Get-Content -path "$topdirectory\$($_.Name)\docs\nav.yml" -Raw
     $newcontent=$template -replace "{replacenavhere}", $nav
     $newcontent=$newcontent -replace "{language}", $lang
-    $newcontent | Set-Content -Path "../$($_.Name)/mkdocs.yml"
+    $ymlfilepath="$topdirectory\$($_.Name)\mkdocs.yml"
+    $newcontent | Set-Content -Path $ymlfilepath
 
-    mkdocs build --config-file ../docs_$lang/mkdocs.yml --site-dir ../$sitedir/$lang
+    mkdocs build --config-file $ymlfilepath --site-dir $sitedir\$lang
 } 
